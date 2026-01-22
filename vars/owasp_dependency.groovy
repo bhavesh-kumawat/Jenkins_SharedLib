ded0bfc221cec9679d630b909b4ea72972bc505e
bhavesh-kumawat <155063327+bhavesh-kumawat@users.noreply.github.com>
2026-01-22 17:26:09 +0530

    Enhance OWASP dependency check with setup commands
    Refactor dependency check process to include cleanup and directory setup.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,20 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+def call() {
+
+  sh '''
+    rm -rf dependency-check-data
+    mkdir -p dependency-check-data
+    chmod -R 777 dependency-check-data
+  '''
+
+  dependencyCheck(
+    odcInstallation: 'OWASP',
+    additionalArguments: '''
+      --scan .
+      --format XML
+      --data dependency-check-data
+      --noupdate
+    '''
+  )
+
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
